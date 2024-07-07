24ea66445d71a35e422fe7b1377702e3b581695f
Riccardo <rigreco@users.noreply.github.com>
2024-07-07 22:52:09 +0200

    Service Worker Cache

--- space-invaders/sw.js
+++ space-invaders/sw.js
@@ -1,12 +1,39 @@
 // sw.js
+
+const CACHE_NAME = 'space-invaders-v1';
+const urlsToCache = [
+  '/space-invaders/',
+  '/space-invaders/index.html',
+  '/space-invaders/space-invaders.js',
+  '/space-invaders/icon.png'
+  // Aggiungi qui altri file che vuoi mettere in cache
+];
+
 self.addEventListener('install', (event) => {
-    console.log('Service Worker installato');
-  });
-  
-  self.addEventListener('activate', (event) => {
-    console.log('Service Worker attivato');
-  });
-  
-  self.addEventListener('fetch', (event) => {
-    console.log('Intercettata una richiesta fetch');
-  });
+  event.waitUntil(
+    caches.open(CACHE_NAME)
+      .then((cache) => {
+        console.log('Cache opened');
+        return cache.addAll(urlsToCache);
+      })
+  );
+  console.log('Service Worker installato');
+});
+
+self.addEventListener('activate', (event) => {
+  console.log('Service Worker attivato');
+});
+
+self.addEventListener('fetch', (event) => {
+  event.respondWith(
+    caches.match(event.request)
+      .then((response) => {
+        if (response) {
+          return response;
+        }
+        return fetch(event.request);
+      }
+    )
+  );
+  console.log('Intercettata una richiesta fetch');
+});
